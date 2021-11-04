3049affd9e0f44b31426efecd10782ddd221568c
fuzzext <goncalomachadoxt@gmail.com>
2021-11-04 12:36:04 -0100

    desenvolvimento git

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
  
-
+app.use(express.static('./public'))
 
 app.use(express.static('./public'))
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile(path.join(__dirname, './public/index.html'))
+
 })
 
 app.get('/:nome', function (req, res) {
